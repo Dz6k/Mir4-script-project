0a5f2988f383624f169c2cef762b3e8c3581a552
Dz6k <pedrosouzadz6@pm.me>
2024-02-13 05:36:28 -0300

    Version 2.1

--- AutoFarm/farm.py
+++ AutoFarm/farm.py
@@ -107,19 +107,25 @@ def farm_die_on_ultimate(game):
     sleep(0.2)
     while im_live == "yes" and not parar_threads:
         try:
-            death = pyautogui.locateOnScreen('AutoFarm\imagens\morto.png', confidence=0.8)
+            death = pyautogui.locateOnScreen('AutoFarm\imagens\morto.png', confidence=0.8,grayscale=False)
             sleep(10)
-            pyautogui.click(death)
-            sleep(10)
+            mkey.left_click_xy_natural(death[0],death[1])
+            #pyautogui.click(death)
+            sleep(7)
             pyautogui.press('f10')
             sleep(1)
-            look_map_x, look_map_y = pyautogui.locateCenterOnScreen('AutoFarm\imagens\map.png', confidence=0.7)
-            pyautogui.click(x=look_map_x, y=look_map_y, duration=0.5)
+            lookmap = pyautogui.locateCenterOnScreen('AutoFarm\imagens\map.png', confidence=0.7,grayscale=False)
+            mkey.left_click_xy_natural(lookmap[0],lookmap[1])
+            # pyautogui.click(x=look_map_x, y=look_map_y, duration=0.5)
             sleep(0.5)
-            energia_x, energia_y = pyautogui.locateCenterOnScreen('AutoFarm\imagens\energia.png', confidence=0.7)
-            pyautogui.click(x=energia_x, y=energia_y, duration=0.5)
-            tp_x, tp_y = pyautogui.locateCenterOnScreen('AutoFarm\imagens\\tp.png', confidence=0.7)
-            pyautogui.click(x=tp_x, y=tp_y, duration=0.5)
+            energia = pyautogui.locateCenterOnScreen('AutoFarm\imagens\energia.png', confidence=0.7,grayscale=False)
+            mkey.left_click_xy_natural(energia[0],energia[1])
+            sleep(1)
+            #pyautogui.click(x=energia_x, y=energia_y, duration=0.5)
+            tp = pyautogui.locateOnScreen('AutoFarm\imagens\\tp.png', confidence=0.6,grayscale=False)
+            
+            mkey.left_click_xy_natural(tp[0],tp[1])
+            #pyautogui.click(tp, duration=0.5)
             sleep(10)
             # n
             win.SendMessage(win32con.WM_KEYDOWN, 0x4E, 0)
@@ -152,7 +158,7 @@ def farm_die_on_ultimate(game):
             win.SendMessage(win32con.WM_KEYUP, 0x52, 0)
     
 def start_simple():
-    instancias = pyautogui.prompt(text='Numero das janela', title='Seletor')
+    instancias = pyautogui.prompt(text='Numero da janela(Apenas uma janela!)', title='Seletor')
     global parar_threads
     parar_threads = False
     if instancias:
